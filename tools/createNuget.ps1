713d37e990c660a2d1db9f0bb09bf220906147aa
Allen Thurman <allen.thurman@perkinelmer.com>
2019-08-21 08:35:38 -0400

    get dll version

--- tools/createNuget.ps1
+++ tools/createNuget.ps1
@@ -1,3 +1,10 @@
 
 
 Write-Host ("Powershell script location is " + $PSScriptRoot)
+
+$ReactiveDomainDll = $PSScriptRoot + "\..\bld\Release\net472\ReactiveDomain.Core.dll"
+
+
+$RDVersion = (Get-Item $ReactiveDomainDll).VersionInfo.FileVersion
+
+Write-Host ("Reactive Domain version is " + $RDVersion)
