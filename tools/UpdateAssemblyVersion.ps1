b4b1b86a4c70ba40c70c6e3970614d98c874dc20
Allen Thurman <allen.thurman@perkinelmer.com>
2019-08-21 13:12:17 -0400

    update powersehll scripts

--- tools/UpdateAssemblyVersion.ps1
+++ tools/UpdateAssemblyVersion.ps1
@@ -1,9 +1,24 @@
+# UpdateAssemblyVersion.ps1
+#
+# This script will check the build.props file to get current assembly version of ReactiveDomain.Core.dll. 
+# This version number will be incremented by 1 before a build if build is off of the master branch
+#
 
+$branch = $env:TRAVIS_BRANCH
+
+# Only update the assembly version on master branch for the sake of creating a new nuget package
+if ($branch -ne "master")  
+{
+  Write-Host ("Not a master branch. Assembly version will remain the same")   
+  Exit
+}
 
 Write-Host ("Powershell script location is " + $PSScriptRoot)
 
 $buildProps = $PSScriptRoot + "\..\src\build.props"
 $props = [xml] (get-content $buildProps)
+
+# Get the assemnbly and file version from build.props
 $assemblyVersionNode = $props.SelectSingleNode("//Project/PropertyGroup/AssemblyVersion")
 $fileVersionNode = $props.SelectSingleNode("//Project/PropertyGroup/FileVersion")
 
@@ -18,15 +33,13 @@ $revision = $assemblyVersionNode.InnerText.Split('.')[3]
 
 Write-Host ("revision is " + $revision )
 
-$global:newRevision++
-
+$newRevision = $newRevision + 1
 Write-Host ("New revision will be is " + $newRevision )
 
 $newAssemblyVersion = $major + "." + $minor + "." + $build + "." + $newRevision
-
 Write-Host ("New assembly version will be is " + $newAssemblyVersion )
 
-#Update the props file
+#Update the props file with the revision number incremented by 1
 $assemblyVersionNode.InnerText = $newAssemblyVersion
 $fileVersionNode.InnerText = $newAssemblyVersion
 
--- tools/createNuget.ps1
+++ tools/createNuget.ps1
@@ -1,13 +1,55 @@
+# UpdateAssemblyVersion.ps1
+#
+# This script will get the fileversion of ReactiveDomain.Core.dll. 
+# This version number will be used to create the corresponding nuget package 
+# The nuget is then pushed to nuget.org
+# 
+# Note: If build is unstable, a beta (pre release) version of the nuget will be pushed
+#       If build is stable, a stable (release) version will be pushed
 
 
+$branch = $env:TRAVIS_BRANCH
+$masterString = "master"
 
-
+# create and push nuget off of master branch
+if ($branch -ne $masterString)  
+{
+  Write-Host ("Not a master branch. Will not create nuget")   
+  Exit
+}
 
 Write-Host ("Powershell script location is " + $PSScriptRoot)
 
 $ReactiveDomainDll = $PSScriptRoot + "\..\bld\Release\net472\ReactiveDomain.Core.dll"
 $RDVersion = (Get-Item $ReactiveDomainDll).VersionInfo.FileVersion
+$nuspec = $PSScriptRoot + "\..\src\ReactiveDomain.nuspec"
+$nuget = $PSScriptRoot + "\..\src\.nuget\nuget.exe"
+$isStable = $env:STABLE
+
 
 Write-Host ("Reactive Domain version is " + $RDVersion)
-Write-Host ("Stable is " + $env:STABLE)
+Write-Host ("Stable is " + $isStable)
+Write-Host ("nuspec file is " + $nuspec)
+Write-Host ("Branch is file is " + $branch)
+
+& $nuget update -self
+
+$versionString = ""
+
+if ($branch -eq $masterString -and $isStable -eq "false" )
+{
+  $versionString = $RDVersion + "-beta"
+  Write-Host ("This is an unstable master build. pushing unstable nuget version: " + $versionString)
+  & $nuget pack $nuspec -Version $versionString
+}
+
+if ($branch -eq $masterString -and $isStable -eq "true" )
+{
+  Write-Host ("This is a stable master build. pushing stable nuget version: " + $RDVersion)
+  & $nuget pack $nuspec -Version $RDVersion
+}
+
+
+
+
 
